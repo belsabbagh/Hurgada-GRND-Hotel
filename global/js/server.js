8594484300bc8923d130213cf52c1b5488c0c31a
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-03-31 07:35:49 +0200

    Created a Node.js web server This server loads a basic index.html file by default.

--- global/js/server.js
+++ global/js/server.js
@@ -1,16 +1,6 @@
+
+
 exports.server_start = function start()
 {
-    const http = require('http');
-    const hostname = "127.0.0.1";
-    const port = 3000;
 
-    server = http.createServer(function(req, res) {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Hello World\n');
-    });
-
-    server.listen(port, hostname, function() {
-        console.log('Server running at https://'+ hostname + ':' + port + '/');
-    });
 }
--- index.js
+++ index.js
@@ -1,6 +1,42 @@
-const server = require("./global/js/server");
-const database = require("./global/js/db-connection");
-let db_connection;
+const mysql = require('mysql'),
+    http = require("http"),
+    fs = require('fs'),
+    hostname = "127.0.0.1",
+    port = 2611;
+let db_connection, server;
 
-server.server_start();
-database.db_connect(db_connection);
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./index.html', null, (error, data) => {
+        if (error) {
+            response.writeHead(404);
+            response.write('file not found');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+};
+
+server = http.createServer(handleRequest);
+server.listen(port, function (error) {
+    if (error) {
+        console.error('Something went wrong ', error);
+        return;
+    }
+    console.log('Server running at http://' + hostname + ':' + port + '/');
+});
+
+db_connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "hurgada-grnd-hotel"
+});
+
+db_connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
