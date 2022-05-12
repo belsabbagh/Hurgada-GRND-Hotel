f910a1f2509a0a60a317c20245392b190b1ad80a
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-05-12 12:27:33 +0200

    Removed some redundant html templates.
    - Replaced by a php function that returns the full html page with given content in it.

--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -1,24 +1,3 @@
-function showbar(){
-    document.getElementById('bar').style.display='inline-block';
-    document.getElementById('icon').style.display='none';
-    document.getElementById('icon2').style.display='inline-block';
-}
-function hidebar(){
-    document.getElementById('icon2').style.display='none';
-    document.getElementById('icon').style.display='inline-block';
-    document.getElementById('bar').style.display='none';
-}
-document.addEventListener('mouseup', function(e) {
-    let container = document.getElementById('bar');
-    if (!container.contains(e.target)) {
-    document.getElementById('bar').style.display='none';
-    document.getElementById('icon2').style.display='none';
-    document.getElementById('icon').style.display='inline-block';
-    }
-  });
-
-
-
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.documentElement.scrollTop > 5) {
@@ -49,32 +28,28 @@ function scrollFunction() {
 }
 
   function reveal() {
-    var reveals = document.querySelectorAll(".reveal")
-    for (var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
-        if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active");
-        } 
-        else {
-            reveals[i].classList.remove("active");
-        }
-    }
-}
+      let reveals = document.querySelectorAll(".reveal");
+      for (let i = 0; i < reveals.length; i++) {
+          let windowHeight = window.innerHeight;
+          let elementTop = reveals[i].getBoundingClientRect().top;
+          let elementVisible = 150;
+          if (elementTop < windowHeight - elementVisible) {
+              reveals[i].classList.add("active");
+          } else {
+              reveals[i].classList.remove("active");
+          }
+      }
+  }
 window.addEventListener("scroll", reveal);
 
 
 
 function show() {
-    var reveals = document.querySelectorAll(".show")
-    for (var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
+    let reveals = document.querySelectorAll(".show");
+    for (let i = 0; i < reveals.length; i++) {
         if (document.documentElement.scrollTop > 5) {
             reveals[i].classList.add("active");
-        } 
-        else{
+        } else {
             reveals[i].classList.remove("active");
         }
     }
