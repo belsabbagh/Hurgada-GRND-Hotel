92b4a8e184d26529ef1c3ced9dfff090e535888d
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-06-10 09:36:08 +0200

    Fixed login to redirect to profile page

--- pages/login/login.php
+++ pages/login/login.php
@@ -6,6 +6,11 @@ try
 {
     if (!post_data_exists()) throw new Exception("Data wasn't submitted correctly", 1);
     log_in($_POST['email'], $_POST['password']);
+    if (active_user_isEmployee())
+    {
+        header("Location: http://localhost/hurgada-grnd-hotel/pages/profile/index.php");
+        return;
+    }
     header("Location: http://localhost/hurgada-grnd-hotel/pages/Home/index.php");
 } catch (Exception $e)
 {
