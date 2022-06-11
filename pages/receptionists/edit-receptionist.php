9673c965a9a010c062b6d14e8042d87bd0d69536
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-06-12 00:25:56 +0200

    Activity logged receptionists

--- pages/receptionists/edit-receptionist.php
+++ pages/receptionists/edit-receptionist.php
@@ -45,6 +45,8 @@ $content = "Operation Successful.";
 try
 {
     edit_receptionist();
+    $user_id = get_active_user_id();
+    activity_log($user_id, "Added new receptionist", "QC with ID:$user_id deleted receptionist with id {$_POST['user_id']}");
     header("Location: index.php");
 } catch (Exception $e)
 {
